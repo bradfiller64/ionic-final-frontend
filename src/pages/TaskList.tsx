ed4ae3b3cbfc69d635a1470d31c65efe317dddab
bradfiller64 <fillerbrad64@gmail.com>
2023-04-01 03:06:34 -0700

    Working on fixing the TaskList component

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,47 +1,72 @@
-import { IonCheckbox, IonItem, IonItemSliding, IonLabel, IonList } from '@ionic/react';
-import React from 'react';
-import { Task } from "../models/task";
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonInput, IonItem, IonItemOptions, IonItemSliding, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState, useContext } from 'react';
+import TaskContext from '../contexts/TaskContext';
 
-interface TaskListProps {
-  tasks: Task[];
-  onToggleTask: (task: Task) => void;
-  onDeleteTask: (task: Task) => void;
-}
+const TaskList = () => {
+  const { tasks, addTask, updateTask, deleteTask } = useContext(TaskContext);
+  const [showAddTaskDialog, setShowAddTaskDialog] = useState(false);
+  const [newTaskName, setNewTaskName] = useState('');
 
-const TaskList: React.FC<TaskListProps> = ({ tasks, onToggleTask, onDeleteTask }) => {
-  const incompleteTasks = tasks.filter((task) => !task.completed);
-  const completedTasks = tasks.filter((task) => task.completed);
+  const handleTaskCheckboxChange = (tasks: { taskId: number, title: string, completed: boolean }) => {
+    const updatedTask = { ...tasks, completed: !tasks.completed };
+    updateTask(updatedTask);
+  };
+
+  const handleDeleteTask = (taskId: number) => {
+    deleteTask(taskId);
+  };
+
+  const handleAddTask = () => {
+    addTask(newTaskName);
+    setShowAddTaskDialog(false);
+    setNewTaskName('');
+  };
+
 
   return (
-    <IonList>
-      <h2>Incomplete Tasks</h2>
-      {incompleteTasks.map((task) => (
-        <IonItemSliding key={task.taskId}>
-          <IonItem>
-            <IonCheckbox
-              checked={task.completed}
-              onIonChange={() => onToggleTask(task)}
-            />
-            <IonLabel>{task.title}</IonLabel>
-          </IonItem>
-        </IonItemSliding>
-      ))}
-
-      <h2>Completed Tasks</h2>
-      {completedTasks.map((task) => (
-        <IonItemSliding key={task.taskId}>
-          <IonItem>
-            <IonCheckbox
-              checked={task.completed}
-              onIonChange={() => onToggleTask(task)}
-            />
-            <IonLabel>{task.title}</IonLabel>
-          </IonItem>
-        </IonItemSliding>
-      ))}
-    </IonList>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Task List</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          {tasks.filter((task: { taskId: number, title: string, completed: boolean }) => !task.completed).map((task: { taskId: number, title: string, completed: boolean }) => (
+            <IonItemSliding key={task.taskId}>
+              <IonItem>
+                <IonLabel>{task.title}</IonLabel>
+                <IonCheckbox slot="end" checked={task.completed} onIonChange={() => handleTaskCheckboxChange(task)} />
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonButton color="danger" onClick={() => handleDeleteTask(task.taskId)}>Delete</IonButton>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
+        <IonButton onClick={() => setShowAddTaskDialog(true)}>Add Task</IonButton>
+        <IonList>
+          {tasks.filter((task: { taskId: number, title: string, completed: boolean }) => task.completed).map((task: { taskId: number, title: string, completed: boolean }) => (
+            <IonItemSliding key={task.taskId}>
+              <IonItem>
+                <IonLabel>{task.title}</IonLabel>
+                <IonCheckbox slot="end" checked={task.completed} onIonChange={() => handleTaskCheckboxChange(task)} />
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonButton color="danger" onClick={() => handleDeleteTask(task.taskId)}>Delete</IonButton>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
+        <IonInput
+          placeholder="New task name"
+          value={newTaskName}
+          onIonChange={(e) => setNewTaskName(e.detail.value as string)}
+        />
+        <IonButton onClick={() => handleAddTask()}>Add</IonButton>
+      </IonContent>
+    </IonPage>
   );
 };
 
 export default TaskList;
-
