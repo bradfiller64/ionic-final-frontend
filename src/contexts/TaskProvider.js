acad39c610d2975f3aef6b329714e718a9e3c0f6
bradfiller64 <fillerbrad64@gmail.com>
2023-04-01 01:39:40 -0700

    get & add functions

--- src/contexts/TaskProvider.js
+++ src/contexts/TaskProvider.js
@@ -2,3 +2,49 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import TaskContext from "./TaskContext";
+
+export const TaskProvider = (props) => {
+
+    const [tasks, setTasks] = useState([]);
+    const baseUrl = "http://localhost:3000/api/tasks/";
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    function getTasks() {
+        axios.get(baseUrl)
+            .then(response => {
+                setTasks(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    function addTask(newTask) {
+        axios.post(baseUrl, { title: newTask, completed: false })
+            .then(response => {
+                setTasks([...tasks, response.data]);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    function updateTask(task) {
+        axios.put(`${apiUrl}/${task.TaskId}`, task)
+            .then(response => {
+                const updatedTasks = tasks.map(t => {
+                    if (t.TaskId === task.TaskId) {
+                        return response.data;
+                    }
+                    return t;
+                });
+                setTasks(updatedTasks);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+};
